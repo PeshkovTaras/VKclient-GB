73e0f39d24556b790b4021cf6de1d74b04b19422
PeshkovTaras <peshkov_taras@mail.ru>
2021-11-17 20:57:11 +1000

    Added Session Singleton

--- VKclient-1/Controllers/MainVC.swift
+++ VKclient-1/Controllers/MainVC.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let session = Session.instance
+        let userName = session.userName.description
+        mainLabel.text = "Добро пожаловать, \(userName)!"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
